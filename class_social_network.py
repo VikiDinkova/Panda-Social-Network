a2d53ec82fed9e552ddc4ae9fd4c9354d79a47dd
AleksandarMitev <pron7o@abv.bg>
2016-01-06 20:31:02 +0200

    add panda

--- class_social_network.py
+++ class_social_network.py
@@ -1,9 +1,22 @@
+SOCIAL_NETWORK = {}
+
+
 class PandaSocialNetwork:
-    def add_panda(self, panda):
+
+    def __init__(self):
         pass
 
+    def add_panda(self, panda):
+        if self.has_panda(panda):
+            return "This Panda already there"
+        else:
+            SOCIAL_NETWORK[panda] = []
+
     def has_panda(self, panda):
-        pass
+        if panda in SOCIAL_NETWORK:  # Проверка дали е в SN
+            return True
+        else:
+            return False
 
     def make_friends(self, panda1, panda2):
         pass
