07e80ca04e1edd12b232bb4769a3da3f786ba5ef
ViksataK <viktor.klisurov@gmail.com>
2016-01-06 20:29:25 +0200

    class panda

---
+++ panda.py
@@ -0,0 +1,44 @@
+class Panda:
+    def __init__(self, name, email, gender):
+        if not self._check_email(email):
+            assert (self._check_email(email) is False)
+            email = input("wrong email\nemail:")
+        self._name = name
+        self._email = email
+        self._gender = gender
+
+    def _check_email(self, stri):
+        return "@pandamail.com" in stri
+
+    def __str__(self):
+        return self._name
+
+    def __hash__(self):
+        return self._name
+
+    def name(self):
+        return self._name
+
+    def email(self):
+        return self._email
+
+    def gender(self):
+        return self._gender
+
+    def isMale(self):
+        return self._gender == "male"
+
+    def isFemale(self):
+        return self._gender == "female"
+
+
+def main():
+    ivo = Panda("Ivo", "ivo@pandamail.com", "male")
+    print(ivo.name())
+    print(ivo.email())
+    print(ivo.gender())
+    print(ivo.isMale())
+    print(ivo.isFemale())
+
+if __name__ == '__main__':
+    main()
