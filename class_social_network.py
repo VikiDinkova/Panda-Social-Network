8bfecc994bbf3e102a10aebce65e107e764451e6
AleksandarMitev <pron7o@abv.bg>
2016-01-06 20:12:29 +0200

    new

---
+++ class_social_network.py
@@ -0,0 +1,15 @@
+class PandaSocialNetwork:
+    def add_panda(self, panda):
+        pass
+
+    def has_panda(self, panda):
+        pass
+
+    def make_friends(self, panda1, panda2):
+        pass
+
+    def are_friends(self, panda1, panda2):
+        pass
+
+    def friends_of(self, panda):
+        pass
